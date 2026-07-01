bc760ed854786a5f7fe9a5e48472360322d1382f
Lucian1972 <30400896+Lucian1972@users.noreply.github.com>
2026-07-01 23:43:45 +0300

    Add files via upload

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("fin.test.rap.lr1.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
